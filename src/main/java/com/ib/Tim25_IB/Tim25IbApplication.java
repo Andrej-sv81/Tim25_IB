1aeea1d17a062e7e03d4a4105b952089d871a6b6
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2023-04-02 22:14:20 +0200

    First commit

---
+++ src/main/java/com/ib/Tim25_IB/Tim25IbApplication.java
@@ -0,0 +1,13 @@
+package com.ib.Tim25_IB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tim25IbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tim25IbApplication.class, args);
+	}
+
+}
