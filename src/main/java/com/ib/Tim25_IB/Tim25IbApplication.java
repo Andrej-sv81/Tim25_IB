33d24ae7754bef62c11e1cf6e7c079ff341ffac2
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2023-04-02 22:40:20 +0200

    Update Tim25IbApplication.java

--- src/main/java/com/ib/Tim25_IB/Tim25IbApplication.java
+++ src/main/java/com/ib/Tim25_IB/Tim25IbApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Tim25IbApplication {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(Tim25IbApplication.class, args);
 	}
 
