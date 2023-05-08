772c0ce1b778c5570b9c5656101c1beff3193b5d
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2023-05-08 18:56:54 +0200

    Validate request

--- src/main/java/com/ib/Tim25_IB/services/CertificateService.java
+++ src/main/java/com/ib/Tim25_IB/services/CertificateService.java
@@ -2,22 +2,19 @@ package com.ib.Tim25_IB.services;
 
 import com.ib.Tim25_IB.DTOs.CertificateDTO;
 import com.ib.Tim25_IB.DTOs.CertificateListDTO;
-import com.ib.Tim25_IB.DTOs.CertificateRequestDTO;
-import com.ib.Tim25_IB.Repository.RequestRepository;
-import com.ib.Tim25_IB.model.CertificateRequest;
-import com.ib.Tim25_IB.model.RequestStatus;
+import com.ib.Tim25_IB.Repository.CertificateRepository;
+import com.ib.Tim25_IB.model.Certificate;
+import com.ib.Tim25_IB.model.CertificateStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
+import java.io.IOException;
 import java.util.List;
 @Service
 public class CertificateService {
 
-
+    @Autowired
+    CertificateRepository certificateRepository;
 
     public CertificateListDTO getAll(){
         List<CertificateDTO> certificateList = null; //get from repo
@@ -25,9 +22,13 @@ public class CertificateService {
         return returnList;
     }
 
-    public void validateCertificate(Long id) {
-        //call repo to validate
-        //get the valid state of the cert with the matching id
+    public boolean validateCertificate(String id) {
+        try {
+            Certificate cert = certificateRepository.findBySerialNumber(id);
+            return cert.getCertificateStatus() == CertificateStatus.VALID;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
